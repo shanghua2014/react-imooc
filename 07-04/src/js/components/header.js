3a4ccce5e89e179f723f363608d93e434f9cde68
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-11-01 15:14:49 +0800

    7-4JSX内置表达式

---
+++ 07-04/src/js/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class HeaderComponent extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>这里一个头部!!!</h1>
+            </header>
+        )
+    }
+}
