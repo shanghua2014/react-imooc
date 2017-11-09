199c072efdde5fa916dcad12125d687695efa226
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-11-09 19:14:44 +0800

    9-2 内联样式中的表达式

---
+++ 09-02/src/js/components/mixins.js
@@ -0,0 +1,8 @@
+const MixinLog = {
+    log ( str ) {
+        console.log('====================================');
+        console.log( str );
+        console.log('====================================');
+    }
+}
+export default MixinLog;
---
+++ 09-02/src/js/index.js
@@ -0,0 +1,29 @@
+/**
+ * 9-2 内联样式中的表达式
+ */
+const React = require('react');
+const ReactDOM = require('react-dom');
+import HeaderComponent from './components/header';
+import FooterComponent from './components/footer';
+import BodyComponent from './components/indexBody'
+
+class Index extends React.Component {
+    componentWillMount () {
+        console.log('------>index页面开始加载。。。');
+    }
+    componentDidMount () {
+        console.log('------>index页面加载完成！');
+    }
+    
+    render() {
+        return (
+            < div >
+                < HeaderComponent / > 
+                <BodyComponent username={'shanghua.zhao'} userphone={15910277440}/>
+                <FooterComponent/>
+            < /div>
+        )
+    }
+}
+
+ReactDOM.render( <Index / >, document.getElementById('box'));
