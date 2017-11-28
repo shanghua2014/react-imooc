d1ba3be10d9fc740a07dbe45bb4af8bdf0e76cbf
shanghua.zhao <shanghua.zhao@huanxi.fm>
2017-11-28 14:22:18 +0800

    13-4 移动端注册功能模块开发

---
+++ 13-04/webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    context:__dirname + '/src',
+    entry: "./js/root.js",
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015'],
+                    plugins: ['react-html-attrs'], //添加组件的插件配置
+                }
+            },
+            //下面是使用 ant-design 的配置文件
+            { 
+                test: /\.css$/, 
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+    output: {
+        path: __dirname,
+        filename: "./src/bundle.js"
+    }
+};
